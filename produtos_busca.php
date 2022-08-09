945dbcf2411ebd725b99fd7dad5a80b24f294b5f
zDeadNow123 <dinnuhgamer@gmail.com>
2022-08-09 15:07:43 -0300

    09-08-2022

--- produtos_busca.php
+++ produtos_busca.php
@@ -1,24 +1,24 @@
 <?php
-    include ('connections/conn.php');
+include('connections/conn.php');
 
-    $busca_user = $_GET['buscar'];
-    //echo "<h1>Você digitou na busca: ".$busca_user."</h1>"
+$busca_user = $_GET['buscar'];
+//echo "<h1>Você digitou na busca: ".$busca_user."</h1>"
 
-    $consulta = "select * from vw_tbprodutos where descri_produto like '%".$busca_user."%' order by descri_produto asc";
-    $lista = $conn -> query($consulta);
-    $linha = $lista -> fetch(PDO::FETCH_ASSOC);
-    $totalLinhas = $lista -> rowCount(); //rowCount() substituindo num_rows
+$consulta = "select * from vw_tbprodutos where descri_produto like '%" . $busca_user . "%' order by descri_produto asc";
+$lista = $conn->query($consulta);
+$linha = $lista->fetch(PDO::FETCH_ASSOC);
+$totalLinhas = $lista->rowCount(); //rowCount() substituindo num_rows
 ?>
 
 <table>
-    <?php do {?>
+    <?php do { ?>
         <tr>
             <td>
-                <?php echo $linha ['descri_produto'] ?>
+                <?php echo $linha['descri_produto'] ?>
             </td>
             <td>
-            <?php echo $linha ['valor_produto'] ?>
+                <?php echo $linha['valor_produto'] ?>
             </td>
         </tr>
-    <?php } while($linha=$lista->fetch(PDO::FETCH_ASSOC))?>
+    <?php } while ($linha = $lista->fetch(PDO::FETCH_ASSOC)) ?>
 </table>
