b856f0d957876873acf78eb9fa05643c635c9ac1
zDeadNow123 <dinnuhgamer@gmail.com>
2022-08-16 15:41:15 -0300

    16-08-2022 - Implementado PHPMailer

--- rodape_contato_envia.php
+++ rodape_contato_envia.php
@@ -21,7 +21,39 @@
                     $nome_contato = $_POST['nome_contato'];
                     $email_contato = $_POST['email_contato'];
                     $msg_contato = "Mensagem de: ".$_POST['nome_contato']."\n".$_POST['comentarios_contato'];
-                    $mailsend = mail($destino, "Formulário de contato Site", $msg_contato, "De: $email_contato");
+
+                    // PHPMailer
+                    use PHPMailer\PHPMailer\PHPMailer;
+
+                    require 'mailer/Exception.php';
+                    require 'mailer/PHPMailer.php';
+                    require 'mailer/SMTP.php';
+
+                    $mail = new PHPMailer();
+                    $mail->isSMTP();
+                    $mail->Host = 'smtp.freesmtpservers.com';
+                    $mail->SMTPAuth = false; // Não desejo autenticar :D
+                    $mail->Port = 25;
+                    //$mail->SMTPSecure = 'tls';
+                    //$mail->Username = $email_contato;
+                    //$mail->Password = 'senha';
+
+
+                    $mail->setFrom($email_contato); // Remetente
+                    $mail->addAddress($destino); // Destinatário
+
+                    $mail->Subject = 'Formulário de contato do Site'; // Assunto
+                    $mail->Body = $msg_contato; // Mensagem
+
+                    if(!$mail->send()) {
+                        echo 'Não foi possível enviar a mensagem.<br>';
+                        echo 'Erro: ' . $mail->ErrorInfo;
+                    } else {
+                        echo 'Mensagem enviada.';
+                    }
+
+                    //$mailsend = mail($destino, "Formulário de contato Site", $msg_contato, "De: $email_contato");
+                    //$mailsend = mail($destino, "Formulário de contato Site", $msg_contato);
                     //echo $nome_contato; (só pra testar)
                     echo "<p class='text-center'>Obrigado por enviar seus comentários, <b>$nome_contato</b></p>";
                     echo "<p class='text-center'>Mensagem enviada com sucesso!</p>";
