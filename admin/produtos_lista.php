86bcac0ad050d0f063f04baf46ad343932f32506
zDeadNow123 <dinnuhgamer@gmail.com>
2022-08-18 13:42:50 -0300

    18-08-2022

--- admin/produtos_lista.php
+++ admin/produtos_lista.php
@@ -24,7 +24,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="../css/meu_estilo.css" type="text/css">;
+    <link rel="stylesheet" href="../css/meu_estilo.css" type="text/css">
     <title>Produtos (<?php echo $total_linhas;?>) - Lista</title>
 </head>
 <body >
