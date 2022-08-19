29b239329a344d1ae4bbf16d9574deb07ece3a9e
zDeadNow123 <dinnuhgamer@gmail.com>
2022-08-19 11:39:47 -0300

    19-08-2022

--- admin/produto_atualiza.php
+++ admin/produto_atualiza.php
@@ -60,7 +60,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo SYS_NAME?> - Alterar</title>
+    <title><?php echo SYS_NAME?> - Admin (Alterar)</title>
 </head>
 <body>
     
