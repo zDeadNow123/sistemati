a957baf2945b5602906d6481635a9819d347f633
zDeadNow123 <dinnuhgamer@gmail.com>
2022-08-19 15:46:38 -0300

    19-08-2022

--- admin/produto_atualiza.php
+++ admin/produto_atualiza.php
@@ -61,8 +61,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SYS_NAME?> - Admin (Alterar)</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/meu_estilo.css" type="text/css">
 </head>
 <body>
-    
+    <?php include('menu_adm.php'); ?>
+
+    <main class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
+                <h2 class="breadcrumb text-danger">
+                    <a href="produto_lista.php">
+                        <button class="btn btn-danger">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </button>
+                    </a>
+                    Atualizando produtos
+                </h2>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
